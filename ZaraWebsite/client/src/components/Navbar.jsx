6d47b10079b1fa5d6d8c9c96f7edcd9b55def5fe
AnujaRajhans <rajhansanuja3@gmail.com>
2024-08-26 19:32:36 +0530

    hello

---
+++ ZaraWebsite/client/src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styled from "styled-components";
+import LogoImg from "../utils/Images/Logo.png";
+import { NavLink } from "react-router-dom";
+
+
+const NavbarContainer = styled.div``;
+const NavLogo = styled.div``;
+const NavItems = styled.div``;
+const Logo = styled.img`
+  width: 100px;
+  height: 50px;`;
+const ButtonContainer = styled.div``;
+
+
+const Navbar = () => {
+  return (
+    <nav>
+      <NavbarContainer>
+        <NavLogo>
+          <Logo src={LogoImg}/>
+        </NavLogo>
+        <NavItems>
+          <NavLink to="/">Home</NavLink><br></br>
+          <NavLink to="/Shop">Shop</NavLink><br></br>
+          <NavLink to="/New_Arrivals">New Arrivals</NavLink><br></br>
+          <NavLink to="/Orders">Orders</NavLink><br></br>
+          <NavLink to="/Contact">Contact</NavLink><br></br>
+        </NavItems>
+        <ButtonContainer>
+         <Button text="SignIn" small/>
+        </ButtonContainer>
+      </NavbarContainer>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ ZaraWebsite/client/src/pages/ShopListing.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ShopListing = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ShopListing
